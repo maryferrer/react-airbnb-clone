dcdb96ee6f3356c23b8bb91f54819f7bf33f3471
maryferrer <85918290+maryferrer@users.noreply.github.com>
2022-01-24 21:11:02 -0800

    Nav component with Airbnb logo

---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <img className="logo" src="./Airbnb_Logo.png" alt="Airbnb logo"></img>
+    </nav>
+  )
+};
+
+export default Navbar;
