4810c5206a6f5a853f3b8efab17296d29b5c49f9
maryferrer <85918290+maryferrer@users.noreply.github.com>
2022-01-25 19:45:22 -0800

    import and render Card.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Navbar from './Navbar';
 import Hero from './Hero';
+import Card from './Card';
 
 ReactDOM.render(
   <React.StrictMode>
     <div>
     <Navbar />,
     <Hero />,
+    <Card 
+      img="./image 12.png" 
+      rating='5.0' 
+      reviewCount={6} 
+      country='USA' 
+      title='Life Lessons with Katie Zaferes' 
+      price={136}
+    />,
     </div>
   </React.StrictMode>,
   document.getElementById('root')
