03a70a65c823eacf8d6dd23fe34119f4edef506d
maryferrer <85918290+maryferrer@users.noreply.github.com>
2022-01-27 11:55:53 -0800

    pass item into props

--- src/Card.js
+++ src/Card.js
@@ -2,23 +2,23 @@ import React from 'react';
 
 function Card(props) {
     let badgeText
-    if (props.openSpots === 0) {
+    if (props.item.openSpots === 0) {
         badgeText = "SOLD OUT"
-    } else if (props.location === "Online") {
+    } else if (props.item.location === "Online") {
         badgeText = "ONLINE"
     }
 
     return (
         <div className="card">
             {badgeText && <div className="card-badge">{badgeText}</div>}
-            <img className="card-img" src={props.img} alt="experiences"></img>
+            <img className="card-img" src={props.item.coverImg} alt="experiences"></img>
                 <div className="card-information">
                     <img className="star" src="./Star 1.png" alt="red star"></img>
-                    <p className="rating">{props.rating}</p> 
-                    <p className="reviews">({props.reviewCount}) • {props.location}</p>
+                    <p className="rating">{props.item.stats.rating}</p> 
+                    <p className="reviews">({props.item.stats.reviewCount}) • {props.item.location}</p>
                 </div>
-            <p className="card-title">{props.title}</p>
-            <p className="price"><b>From ${props.price}</b> / person</p>
+            <p className="card-title">{props.item.title}</p>
+            <p className="price"><b>From ${props.item.price}</b> / person</p>
         </div>
     )
 };
