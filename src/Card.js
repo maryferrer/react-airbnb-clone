183f12fad89503c38f5b72117ded005e4d7393f8
maryferrer <85918290+maryferrer@users.noreply.github.com>
2022-01-26 19:23:36 -0800

    add badgeText, revise props to be consistent with data.js file

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
 function Card(props) {
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+
     return (
         <div className="card">
-            <img className="card-img" src={props.img} alt="Katie Zaferes"></img>
+            {badgeText && <div className="card-badge">{badgeText}</div>}
+            <img className="card-img" src={props.img} alt="experiences"></img>
                 <div className="card-information">
                     <img className="star" src="./Star 1.png" alt="red star"></img>
                     <p className="rating">{props.rating}</p> 
-                    <p className="reviews">({props.reviewCount}) • {props.country}</p>
+                    <p className="reviews">({props.reviewCount}) • {props.location}</p>
                 </div>
             <p className="card-title">{props.title}</p>
             <p className="price"><b>From ${props.price}</b> / person</p>
